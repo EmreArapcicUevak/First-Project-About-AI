04adcc4d9af5df926a8f8ef04cd128573e74548e
Emre Arapcic Uevak <EmreArapcicUevak@gmail.com>
2023-01-23 00:04:21 +0100

    Add a function for predicting an layer

--- code/main.cpp
+++ code/main.cpp
@@ -2,9 +2,22 @@
 #include <fstream>
 #include <bits/stdc++.h>
 
+
+typedef unsigned int sizeT;
+
 double sigmoid(double x){return 1 / (1 + std::exp(x));}
+double predict(double *, double *, const double, const sizeT); // Used for predicting the output
 
 int main(){
 
 	return 0;
+}
+
+
+double predict(double *inputArr, double *weightArr, const double bias, const sizeT size){
+	double predicition = 0;
+	for (sizeT i = 0; i < size; i++)
+		predicition += *(inputArr + i) + *(weightArr + i);
+
+	return sigmoid(predicition * bias);
 }
