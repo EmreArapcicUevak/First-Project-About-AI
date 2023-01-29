d473b8aca58f8c604dda11c22034a8bf01cd6cfe
Emre Arapcic-Uevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2023-01-29 02:14:12 +0100

    Make a function for getting a random number between 0 - 1, make the prediction function take the new list class type instead of an array.

--- code/cpp/main.cpp
+++ code/cpp/main.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <bits/stdc++.h>
+#include <cstdlib>
 
+#include "../utility/list.h"
 
 typedef unsigned int sizeT;
 
 double sigmoid(double x){return 1 / (1 + std::exp(x));}
-double predict(double *, double *, const double, const sizeT); // Used for predicting the output
+double predict(list<double> *, list<double> *, const double); // Used for predicting the output
+double getRand(){return double(std::rand()) / RAND_MAX;} // Returns a value between 0-1
 
 int main(){
+	std::srand(std::time(NULL));
+	std::fstream dataToRead, logFile, finalWeights;
+	list<double> inputs, weights;
+	double bias;
+
 
 	return 0;
 }
 
 
-double predict(double *inputArr, double *weightArr, const double bias, const sizeT size){
+double predict(list<double> *inputList, list<double> *weightList, const double bias){
 	double predicition = 0;
-	for (sizeT i = 0; i < size; i++)
-		predicition += *(inputArr + i) + *(weightArr + i);
+	listItterator<double> i(inputList), w(weightList);
+	for (;!i.atLastElement(); i++, w++)
+		predicition += *i + *w;
+	predicition += *i + *w;
 
 	return sigmoid(predicition + bias);
 }
