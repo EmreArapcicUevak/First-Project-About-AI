deacdadd20f7d859c2edf9acd7078727be8d3ffc
Emre Arapcic-Uevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2023-01-30 01:52:59 +0100

    Improve plotting code

--- code/OctaveMatlab/plotError.m
+++ code/OctaveMatlab/plotError.m
@@ -1,6 +1,10 @@
 Data = load('-ascii', '../../data/log.txt');
 Data = Data';
 
+close all;
+
 t = linspace(0,1, length(Data));
 
-plot(t,Data, '--r');
+plot(t,Data, 'r', 'linewidth', 2); grid on; grid minor on;
+title('Cost/Error'); ylabel ('Error'); xlabel ('Training %');
+set(gca, "fontsize", 15);
